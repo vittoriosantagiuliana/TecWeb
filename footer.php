f85b2fd38ac05b7c9e81d5702a45519d22ea4f5e
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2019-12-01 18:44:23 +0100

    footer rotto, include script mancanti

--- footer.php
+++ footer.php
@@ -1,12 +1,24 @@
 <div id="footer">
 	<p>Seguici sui social per tutte le novità sul parco!</p>
 	<div id="social">
-		<a href="#" class="fa fa-facebook" title="Facebook"></a>
-		<a href="#" class="fa fa-twitter" title="Twitter"></a>
-		<a href="#" class="fa fa-google" title="Google+"></a>
-		<a href="#" class="fa fa-linkedin" title="Linkedin"></a>
-		<a href="#" class="fa fa-instagram" title="Instagram"></a>
-		<a href="#" class="fa fa-pinterest" title="Pinterest"></a>
+		<div class="socialButton">
+			<a href="#" class="fa fa-facebook" title="Facebook"></a>
+		</div>
+		<div class="socialButton">
+			<a href="#" class="fa fa-twitter" title="Twitter"></a>
+		</div>
+		<div class="socialButton">
+			<a href="#" class="fa fa-google" title="Google+"></a>
+		</div>
+		<div class="socialButton">
+			<a href="#" class="fa fa-linkedin" title="Linkedin"></a>
+		</div>
+		<div class="socialButton">
+			<a href="#" class="fa fa-instagram" title="Instagram"></a>
+		</div>
+		<div class="socialButton">
+			<a href="#" class="fa fa-pinterest" title="Pinterest"></a>
+		</div>
 	</div>
 	<p><span xml:lan="en">All rights reserved</span> - Alessio Barbiero, Federico Caretta, Matteo Lattanzio, Vittorio Santagiuliana</p>
 	<p> Zoo Creola, Via delle Seghe 69 Saccolongo(PD), 049xxxxxxx</p>
