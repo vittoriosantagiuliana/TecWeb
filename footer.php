8001de9cfe50d3c4dceabb92b0aef5bcf4e040b3
Cionsa <daimatty@gmail.com>
2019-12-12 17:18:23 +0100

    Cambiate icone social

--- footer.php
+++ footer.php
@@ -1,25 +1,27 @@
 <div id="footer">
 	<p>Seguici sui social per tutte le novità sul parco!</p>
 	<div id="social">
-		<div class="socialButton">
-			<a href="#" class="fa fa-facebook" title="Facebook"></a>
-		</div>
-		<div class="socialButton">
-			<a href="#" class="fa fa-twitter" title="Twitter"></a>
-		</div>
-		<div class="socialButton">
-			<a href="#" class="fa fa-google" title="Google+"></a>
-		</div>
-		<div class="socialButton">
-			<a href="#" class="fa fa-linkedin" title="Linkedin"></a>
-		</div>
-		<div class="socialButton">
-			<a href="#" class="fa fa-instagram" title="Instagram"></a>
-		</div>
-		<div class="socialButton">
-			<a href="#" class="fa fa-pinterest" title="Pinterest"></a>
+		<div id="socialContainer">
+			<div class="socialButton">
+          	  <a href="#"><img class="socialImg" src="./images/Facebook.png" title="Facebook"/></a>
+			</div>
+			<div class="socialButton">
+         	   <a href="#"><img class="socialImg" src="./images/WhatsApp.png" title="Whatsapp"/></a>
+			</div>
+			<div class="socialButton">
+           		 <a href="#"><img class="socialImg" src="./images/Linkedin.png" title="Linkedin"/></a>
+			</div>
+			<div class="socialButton">
+           	 	<a href="#"><img class="socialImg" src="./images/Instagram.png" title="Instagram"/></a>
+			</div>
+			<div class="socialButton">
+            	<a href="#"><img class="socialImg" src="./images/Twitter.png" title="Twitter"/></a>
+			</div>
+			<div class="socialButton">
+            	<a href="#"><img class="socialImg" src="./images/Pinterest.png" title="Pinterest"/></a>
+			</div>
 		</div>
 	</div>
 	<p><span xml:lan="en">All rights reserved</span> - Alessio Barbiero, Federico Caretta, Matteo Lattanzio, Vittorio Santagiuliana</p>
-	<p> Zoo Creola, Via delle Seghe 69 Saccolongo(PD), 049xxxxxxx</p>
+	<p> Zoo Creola, Via Montagnola di Caretta 1 Saccolongo(PD), 049xxxxxxx</p>
 </div>
