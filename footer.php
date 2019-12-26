8a1c6e76395041d160bb869e2a1c633f24638d7b
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2019-12-27 00:51:37 +0100

    Non va un cazzo

--- footer.php
+++ footer.php
@@ -1,27 +1,7 @@
-<div id="footer">
-	<p>Seguici sui social per tutte le novità sul parco!</p>
-	<div id="social">
-		<div id="socialContainer">
-			<div class="socialButton">
-          	  <a href="#"><img class="socialImg" src="./images/Facebook.png" title="Facebook"/></a>
-			</div>
-			<div class="socialButton">
-         	   <a href="#"><img class="socialImg" src="./images/WhatsApp.png" title="Whatsapp"/></a>
-			</div>
-			<div class="socialButton">
-           		 <a href="#"><img class="socialImg" src="./images/Linkedin.png" title="Linkedin"/></a>
-			</div>
-			<div class="socialButton">
-           	 	<a href="#"><img class="socialImg" src="./images/Instagram.png" title="Instagram"/></a>
-			</div>
-			<div class="socialButton">
-            	<a href="#"><img class="socialImg" src="./images/Twitter.png" title="Twitter"/></a>
-			</div>
-			<div class="socialButton">
-            	<a href="#"><img class="socialImg" src="./images/Pinterest.png" title="Pinterest"/></a>
-			</div>
-		</div>
-	</div>
-	<p><span xml:lan="en">All rights reserved</span> - Alessio Barbiero, Federico Caretta, Matteo Lattanzio, Vittorio Santagiuliana</p>
-	<p> Zoo Creola, Via Montagnola di Caretta 1 Saccolongo(PD), 049xxxxxxx</p>
-</div>
+<?php
+class Footer {
+	public static function build() {
+		return file_get_contents("html/footer.html");
+	}
+}
+?>
--- index.php
+++ index.php
@@ -1,31 +1,10 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<title>Zoo</title>
-		<meta name="title" content="Zoo"/>
-		<meta name="description" content="Home page dello zoo"/>
-		<meta name="keywords" content="zoo, tecnologie web, progetto, animali"/>
-		<meta name="language" content="italian it"/>
-		<meta name="author" content="Alessio Barbiero"/>
-        <link href="https://fonts.googleapis.com/css?family=Fredoka One" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
-		<link rel="stylesheet" href="css/style.css"/>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-		<script type="text/javascript" src="script/script.js"></script>
-	</head>
-	<body>
-		<?php include("header.php"); ?>
-		<div id="content">
-			<div id="backgroundContent">
-				<!--<img class="indexBackground" src="images/indexBackground.jpg" alt="immagine di copertina"/>-->
-			</div>
-			<div id="imgBar">
-				<a href="map.php"><img src="images/barMap.jpg" alt="mappa"/></a>
-				<a href="schedule.php"><img src="images/barSchedule.jpg" alt="orari"/></a>
-				<a href="ticket.php"><img src="images/barTicket.jpg" alt="biglietti"/></a>
-			</div>
-		</div>
-		<?php include("footer.php"); ?>
-	</body>
-</html>
+﻿<?php
+	require_once "header.php";
+	require_once "footer.php";
+
+	$output = file_get_contents("html/index.html");
+	#$output = str_replace('<div id="header"></div>', Header::build(), $output);
+	#$output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
+
+	echo $output;
+?>
