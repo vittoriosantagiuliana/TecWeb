8e23e2dc6b62f5bbb96f350bf38e60e00096e1c6
Cionsa <daimatty@gmail.com>
2020-01-04 12:02:17 +0100

    Corretto maps.php

--- maps.php
+++ maps.php
@@ -1,40 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-        <title>Zoo - Come raggiungerci</title>
-        <link rel="icon" href="images/logo.png">
-		<meta name="title" content="Zoo"/>
-		<meta name="description" content="Animali dello zoo"/>
-		<meta name="keywords" content="zoo, tecnologie web, progetto, animali"/>
-		<meta name="language" content="italian it"/>
-		<meta name="author" content="Alessio Barbiero"/>
-		<link href="https://fonts.googleapis.com/css?family=Fredoka One" rel="stylesheet"/>
-		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
-		<link href="https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/style.css"/>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-		<script type="text/javascript" src="script/script.js"></script>
-	</head>
-<body>
-    <?php include("header.php"); ?>
-    <div id="breadcrumb">
-			<p><a href="index.html">Home</a> &#124; Come raggiungerci</p>
-        </div>
-    <div id="content">
-    <div id="googleMaps"> 
-        <h1>Come raggiungerci</h1>
-        <div id="mapText">
-            <p> Zoo Creola<br> Via Montagnola di Caretta 1<br> Saccolongo(PD)<br> 049xxxxxxx<br></p>
-        </div>
-        <div id="mapMap">
-            <div class="mapouter">
-                <div class="gmap_canvas">
-                    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=montagnola%20di%20caretta&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.bitgeeks.net"></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-    <?php include("footer.php"); ?>
-</body>
+<?php
+	require_once "header.php";
+	require_once "footer.php";
+
+	$output = file_get_contents("html/maps.html");
+	$output = str_replace('<div id="header"></div>', Header::build(), $output);
+	$output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
+
+	echo $output;
+?>
