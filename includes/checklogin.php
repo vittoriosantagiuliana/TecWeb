075f846966118a502bd9f5331d301a3357a9b08c
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-17 16:32:11 +0100

    Sessioni utente accompagnatore

--- includes/checklogin.php
+++ includes/checklogin.php
@@ -12,19 +12,21 @@
 			$username=mysqli_real_escape_string($connessione,$_POST["user"]);
 			$password=mysqli_real_escape_string($connessione,$_POST["password"]);
 			$sql="SELECT * FROM $tab_name WHERE Username_Ut='$username' and Password_Ut='$password'";
-			$result=$connessione->query($sql);
-			if(!$result){
-				echo "Errore della query: ".$connessione->error;
-				exit();
-			}
-			$conta=mysqli_num_rows($result);
-			if($conta==1){
+			if(!$result = $connessione->query($sql);)
+				exit("Errore della query: " . $connessione->error);
+			if(mysqli_num_rows($result) == 1) {
 				if (!isset($_SESSION)) { session_start(); }
 				$_SESSION['userName'] = $username;
 				$_SESSION['userType'] = ($username == "admin") ? "admin" : "user";
+
+				$sql = "SELECT UsernameUt_UA FROM utenteaccompagnatore WHERE UsernameUt_UA=$username";
+				if(!$result = $connessione->query($sql);)
+					exit("Errore della query: " . $connessione->error);
+				$_SESSION["UtenteAccompagnatore"] = mysqli_num_rows($result) == 1;
+
 				header("Location: index.php");
 				exit();
-			}else{
+			} else {
 				$error_m="LOGIN FALLITO! Per favore riprova con altre credenziali oppure REGISTRATI";
 			}
 		}
