23c8c890f5bf0da942e17e069db29eddf534de94
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-17 16:57:07 +0100

    Errore ';' su checklogin

--- includes/checklogin.php
+++ includes/checklogin.php
@@ -12,18 +12,16 @@
 			$username=mysqli_real_escape_string($connessione,$_POST["user"]);
 			$password=mysqli_real_escape_string($connessione,$_POST["password"]);
 			$sql="SELECT * FROM $tab_name WHERE Username_Ut='$username' and Password_Ut='$password'";
-			if(!$result = $connessione->query($sql);)
+			if(!$result = $connessione->query($sql))
 				exit("Errore della query: " . $connessione->error);
 			if(mysqli_num_rows($result) == 1) {
 				if (!isset($_SESSION)) { session_start(); }
 				$_SESSION['userName'] = $username;
 				$_SESSION['userType'] = ($username == "admin") ? "admin" : "user";
-
 				$sql = "SELECT UsernameUt_UA FROM utenteaccompagnatore WHERE UsernameUt_UA=$username";
-				if(!$result = $connessione->query($sql);)
+				if(!$result = $connessione->query($sql))
 					exit("Errore della query: " . $connessione->error);
 				$_SESSION["UtenteAccompagnatore"] = mysqli_num_rows($result) == 1;
-
 				header("Location: index.php");
 				exit();
 			} else {
