f348f4b38c6596b80206876940c1835480f4406b
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-24 21:50:06 +0100

    Divisione comportamento php contattaci/sendmessage

--- contacts.php
+++ contacts.php
@@ -2,22 +2,32 @@
 
 	require_once "includes/header.php";
 	require_once "includes/footer.php";
-	require_once "sendmessage.php";
+	require_once "includes/dbhandler.php";
 	if (!isset($_SESSION)) {
 		session_start();
 	}
 
-	$inviato = isset($done) ? "<h2>" . $done . "</h2>" : "";
-	
+	$inviato = isset($_GET["done"]) ? "<h2>Grazie del tuo messaggio! Verrai ricontattato al pi&ugrave; presto!</h2>" : "";
+	$inviato = isset($_GET["error"]) ? "<h2>" . $_GET["done"] . "</h2>" : "";
+
+	$valueUser = "";
+	$valueEmail = "";
+	if (isset($_SESSION["userName"])) {
+        $userName = $_SESSION["userName"];
+        $result = $connessione->query("SELECT Nome_Ut,Mail_Ut FROM utente WHERE Username_Ut='$userName';");
+        $user = $result->fetch_assoc();
+        $name = $user["Nome_Ut"];
+        $email = $user["Mail_Ut"];
+        $valueUser = "value=\""  . $name . "\"";
+        $valueEmail =  "value=\""  . $email . "\"";
+    }
 
 	$output = file_get_contents("html/contacts.html");
 	$output = str_replace("<div id=\"header\"></div>", Header::build(), $output);
 	$output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
 	$output = str_replace("<h2 inviato/>", $inviato, $output);
-	if (isset($_SESSION["userName"])) {
-		$output = str_replace("%ValueUsername%", $name, $output);
-		$output = str_replace("%ValueEmail%", $email, $output);
-	}
+	$output = str_replace("%ValueUsername%", $valueUser, $output);
+	$output = str_replace("%ValueEmail%", $valueEmail, $output);
 
 	echo $output;
 
--- sendmessage.php
+++ sendmessage.php
@@ -1,32 +1,26 @@
 <?php
+	require_once "includes/dbhandler.php";
+	$connessione = connessione();
 	if (!isset($_SESSION)) {
 		session_start();
 	}
 
-	require_once "includes/dbhandler.php";
-    $connessione = connessione();
-    
-    if (isset($_SESSION['userName'])) {
-        $un = $_SESSION['userName'];
-        $user = $connessione->query("SELECT Nome_Ut,Mail_Ut FROM utente WHERE Username_Ut='$un';");
-        $user = mysqli_fetch_array($user);
-        $name = $user['Nome_Ut'];
-        $email = $user['Mail_Ut'];
-    }
-    if (isset($_POST['form_button'])) {
-        if (!isset($_SESSION['userName'])) {
-            $name = mysqli_real_escape_string($connessione, $_POST['name']);
-            $email = mysqli_real_escape_string($connessione, $_POST['email']);
-        }
-        $message = mysqli_real_escape_string($connessione, $_POST['message']);
-        $cat = mysqli_real_escape_string($connessione, $_POST['subject']);
-        $today = new DateTime();
-        $sql = ("INSERT INTO messaggio(Categoria_Mes,Nome_Mes,Mail_Mes,Testo_Mes) VALUES ('$cat','$name','$email','$message','$today');");
-        $result = $connessione->query($sql);
-        if (!$result) {
-            echo "Errore della query: ".$connessione->error;
-            exit();
-        } else {
-            $done = "Grazie del tuo messaggio! Verrai ricontattato al pi&ugrave; presto!";
-        }
-    }
+	if (isset($_POST['form_button'])) {
+		if (!isset($_SESSION['userName'])) {
+			$name = mysqli_real_escape_string($connessione, $_POST['name']);
+			$email = mysqli_real_escape_string($connessione, $_POST['email']);
+		}
+		$message = mysqli_real_escape_string($connessione, $_POST['message']);
+		$cat = mysqli_real_escape_string($connessione, $_POST['subject']);
+		$today = new DateTime();
+		$sql = ("INSERT INTO messaggio(Categoria_Mes,Nome_Mes,Mail_Mes,Testo_Mes) VALUES ('$cat','$name','$email','$message','$today');");
+		if ($result = $connessione->query($sql)) {
+			header("Location: contacts.php?done");
+			exit();
+		}
+		else {
+			header("Location: contacts.php?error=" . urldecode($connessione->error));
+			exit();
+		}
+
+	}
