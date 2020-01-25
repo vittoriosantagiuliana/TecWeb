5b20a14ef12c46faa7be799135b04ca50af60a71
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-25 01:22:40 +0100

    Pagine admin e user non accessibili ad utenti non loggati (redirect)

--- admin.php
+++ admin.php
@@ -1,6 +1,15 @@
 <?php
 	require_once "includes/header.php";
 	require_once "includes/footer.php";
+	if (!isset($_SESSION)) {
+		session_start();
+	}
+	if (!isset($_SESSION["userName"])) {
+		header("Location: home.php");
+	}
+	if ($_SESSION["userType"] != "admin") {
+		header("Location: user.php");
+	}
 
 	$output = file_get_contents("html/admin.html");
 	$output = str_replace("<div id=\"header\"></div>", Header::build(), $output);
