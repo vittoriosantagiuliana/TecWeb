800510ec452810f47f3056bc83c35072d7272b99
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-27 15:51:02 +0100

    Animali validato

--- animals.php
+++ animals.php
@@ -18,7 +18,7 @@ function listaAnimali() {
 		$output .= "<div class=\"animalsImg\">
 			 <p>" . $nomeAnimale . "</p>
 			 <a href=\"animaldetails.php?animal=" . urlencode($nomeAnimale) . "\">
-				 <img class=\"transiction\" src=\"data:image/jpeg;base64," . base64_encode($animale["Immagine_An"]) . "\" alt=\"" . $nomeAnimale . "\">
+				 <img class=\"transiction\" src=\"data:image/jpeg;base64," . base64_encode($animale["Immagine_An"]) . "\" alt=\"" . $nomeAnimale . "\"/>
 			 </a>
 		 </div>";
 	}
@@ -31,31 +31,31 @@ if (isset($_SESSION["userName"]) && $_SESSION["userType"]=="admin")
 			<legend>Aggiungi un nuovo animale</legend>
 			<div>
 				<label for=\"nomeA\">Nome Comune: </label>
-				<input type=\"text\" name=\"nomeA\"/>
+				<input type=\"text\" name=\"nomeA\" id=\"nomeA\"/>
 			</div>
 			<div>
 				<label for=\"scieA\">Nome Scientifico: </label>
-				<input type=\"text\" name=\"scieA\"/>
+				<input type=\"text\" name=\"scieA\" id=\"scieA\"/>
 			</div>
 			<div>
 				<label for=\"ordA\">Ordine: </label>
-				<input type=\"text\" name=\"ordA\"/>
+				<input type=\"text\" name=\"ordA\" id=\"ordA\"/>
 			</div>
 			<div>
 				<label for=\"famA\">Famiglia: </label>
-				<input type=\"text\" name=\"famA\"/>
+				<input type=\"text\" name=\"famA\" id=\"famA\"/>
 			</div>
 			<div>
 				<label for=\"habA\">Habitat: </label>
-				<textarea name=\"habA\" cols=\"30\" rows=\"2\"></textarea>
+				<textarea name=\"habA\" id=\"habA\" cols=\"30\" rows=\"2\"></textarea>
 			</div>
 			<div>
 				<label for=\"ripA\">Riproduzione: </label>
-				<textarea name=\"ripA\" cols=\"30\" rows=\"2\"></textarea>
+				<textarea name=\"ripA\" id=\"ripA\" cols=\"30\" rows=\"2\"></textarea>
 			</div>
 			<div>
 				<label for=\"curioA\">Curiosit&agrave;: </label>
-				<textarea name=\"curioA\" cols=\"30\" rows=\"2\"></textarea>
+				<textarea name=\"curioA\" id=\"curioA\" cols=\"30\" rows=\"2\"></textarea>
 			</div>
 			<div>
 				<label for=\"imgA\">Inserisci un'immagine per l'animale: </label>
