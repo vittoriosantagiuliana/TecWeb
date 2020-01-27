e603df6ced29a4feea8b2b332a0218fc044e2093
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-27 11:56:46 +0100

    Fix form attività

--- activities.php
+++ activities.php
@@ -1,48 +1,47 @@
 <?php
-    require_once "includes/header.php";
-    require_once "includes/footer.php";
-    require_once "includes/checkatt.php";
-    require_once "includes/dbhandler.php";
-    $connessione = connessione();
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+	require_once "includes/header.php";
+	require_once "includes/footer.php";
+	require_once "includes/checkatt.php";
+	require_once "includes/dbhandler.php";
+	$connessione = connessione();
+	if (!isset($_SESSION)) {
+		session_start();
+	}
 
 function listaAttivita()
 {
-    global $connessione;
+	global $connessione;
 
-    $listaAtt=$connessione->query("SELECT Nome_Att, Descrizione_Att, Immagine_Att FROM attivita;");
-    
-    $attivita = "";
-    while ($singolaAtt = $listaAtt->fetch_assoc()) {
-        $attivita .=
-        "<div class=\"activitySection\">
+	$listaAtt=$connessione->query("SELECT Nome_Att, Descrizione_Att, Immagine_Att FROM attivita;");
+	
+	$attivita = "";
+	while ($singolaAtt = $listaAtt->fetch_assoc()) {
+		$attivita .=
+		"<div class=\"activitySection\">
 			<img src=\"data:image/jpeg;base64,%ImgB64%\" alt=\"%NomeAtt%\"/>
 			<strong>%NomeAtt%</strong>
 			<p>%DescrizioneAtt%</p>
 		</div>";
 
-        $attivita = str_replace("%NomeAtt%", $singolaAtt["Nome_Att"], $attivita);
-        $attivita = str_replace("%ImgB64%", base64_encode($singolaAtt["Immagine_Att"]), $attivita);
-        $attivita = str_replace("%DescrizioneAtt%", $singolaAtt["Descrizione_Att"], $attivita);
-    }
+		$attivita = str_replace("%NomeAtt%", $singolaAtt["Nome_Att"], $attivita);
+		$attivita = str_replace("%ImgB64%", base64_encode($singolaAtt["Immagine_Att"]), $attivita);
+		$attivita = str_replace("%DescrizioneAtt%", $singolaAtt["Descrizione_Att"], $attivita);
+	}
 
-    return $attivita;
+	return $attivita;
 }
 
 function formAttivita()
 {
-    global $connessione;
-    $form = "";
-    $username = isset($_SESSION["userName"]) ? $_SESSION["userName"] : "";
-
-    if (isset($_SESSION["userName"]) && $_SESSION["UtenteAccompagnatore"]) {
-        $connessione->query("DROP VIEW IF EXISTS gruppoProva;
+	global $connessione;
+	$form = "";
+	if (isset($_SESSION["userName"]) && $_SESSION["UtenteAccompagnatore"]) {
+		$username = $_SESSION["userName"];
+		$connessione->query("DROP VIEW IF EXISTS gruppoProva;
 			CREATE VIEW gruppoProva AS SELECT UsernameUt_UA, ID_Gr, NumPers_Gr FROM gruppo, utenteaccompagnatore WHERE UsernameUt_UA='$username' AND IDGr_UA=ID_Gr;");
-        $gruppo = $connessione->query("SELECT GP.UsernameUT_UA AS UsernameUT_UA, GP.ID_Gr AS ID_Gr,GP.NumPers_Gr AS NumPers_Gr,C.Nome_C AS Nome_C,C.NomeIst_C AS NomeIst_C FROM gruppoProva as GP LEFT JOIN classe as C on GP.ID_Gr=C.IDGr_C ORDER BY GP.ID_Gr");
+		$gruppo = $connessione->query("SELECT GP.UsernameUT_UA AS UsernameUT_UA, GP.ID_Gr AS ID_Gr,GP.NumPers_Gr AS NumPers_Gr,C.Nome_C AS Nome_C,C.NomeIst_C AS NomeIst_C FROM gruppoProva as GP LEFT JOIN classe as C on GP.ID_Gr=C.IDGr_C ORDER BY GP.ID_Gr");
 
-        $form = "<form method=\"post\" action=\"activities.php\" id=\"registraAtt\">
+		$form = "<form method=\"post\" action=\"activities.php\" id=\"registraAtt\">
 					<fieldset>
 						<legend>Iscrivi uno dei tuoi gruppi alle attivit&agrave;</legend>
 						<label>Scegli una delle attivit&agrave;</label>
@@ -59,31 +58,31 @@ function formAttivita()
 					</fieldset>
 				</form>";
 
-        $selectAttivita = "";
-        while ($attiv = mysqli_fetch_array($attivita)) {
-            $selectAttivita .= "<option value=\"" . $attiv["ID_Att"] . "\">" . $attiv["Nome_Att"] . "</option>";
-        }
-        
-        $selectGruppo = "";
-        while ($gr = mysqli_fetch_array($gruppo)) {
-            $selectGruppo .= "<option value=\"" . $gr["ID_Gr"] . "\">";
-            if ($gr['Nome_C'] == null) {
-                $selectGruppo .= "Gruppo di " . $gr["NumPers_Gr"] . " persone";
-            } else {
-                $selectGruppo .= "Classe " . $gr["Nome_C"] . " dell'istituto " . $gr["NomeIst_C"] . " (" . $gr["NumPers_Gr"] . " persone)";
-            }
-            $selectGruppo .= "</option>";
-        }
+		$selectAttivita = "";
+		while ($attiv = mysqli_fetch_array($attivita)) {
+			$selectAttivita .= "<option value=\"" . $attiv["ID_Att"] . "\">" . $attiv["Nome_Att"] . "</option>";
+		}
+		
+		$selectGruppo = "";
+		while ($gr = mysqli_fetch_array($gruppo)) {
+			$selectGruppo .= "<option value=\"" . $gr["ID_Gr"] . "\">";
+			if ($gr['Nome_C'] == null) {
+				$selectGruppo .= "Gruppo di " . $gr["NumPers_Gr"] . " persone";
+			} else {
+				$selectGruppo .= "Classe " . $gr["Nome_C"] . " dell'istituto " . $gr["NomeIst_C"] . " (" . $gr["NumPers_Gr"] . " persone)";
+			}
+			$selectGruppo .= "</option>";
+		}
 
-        $erroreData = isset($errorD) ? "<div class=\"error\">" . $errorD . "</div>" : "";
-        
-        $form = str_replace("%SelectAttivita%", $selectAttivita, $form);
-        $form = str_replace("%SelectGruppo%", $selectGruppo, $form);
-        $form = str_replace("%ErroreData%", $erroreData, $form);
-    }
+		$erroreData = isset($errorD) ? "<div class=\"error\">" . $errorD . "</div>" : "";
+		
+		$form = str_replace("%SelectAttivita%", $selectAttivita, $form);
+		$form = str_replace("%SelectGruppo%", $selectGruppo, $form);
+		$form = str_replace("%ErroreData%", $erroreData, $form);
+	}
 
-    if (isset($_SESSION["userName"]) && $_SESSION["userType"] == "admin") {
-        $form = "<form method=\"post\" action=\"activities.php\" id=\"addActivity\" enctype=\"multipart/form-data\">
+	if (isset($_SESSION["userName"]) && $_SESSION["userType"] == "admin") {
+		$form = "<form method=\"post\" action=\"activities.php\" id=\"addActivity\" enctype=\"multipart/form-data\">
 				<fieldset>
 					<legend>Aggiungi una nuova attivit&agrave;</legend>
 					<label for=\"nomeAtt\">Nome attivit&agrave;: </label>
@@ -95,17 +94,19 @@ function formAttivita()
 					<input type=\"submit\" name=\"addAtt\" value=\"Aggiungi attivit&agrave;\"/>
 				</fieldset>
 			</form>";
-    }
-    return $form;
+	}
+	return $form;
 }
 
-    $output = file_get_contents("html/activities.html");
-    $output = str_replace("<div id=\"header\"></div>", Header::build(), $output);
-    $output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
+	$output = file_get_contents("html/activities.html");
+	$output = str_replace("<div id=\"header\"></div>", Header::build(), $output);
+	$output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
 
-    $prenotazioneEffettuata = isset($done) ? "<div class=\"done\"><h2>" . $done . "</h2></div>" : "";
-    $output = str_replace("<h2 prenotazione/>", $prenotazioneEffettuata, $output);
-    $output = str_replace("<div class=\"activitySection\"/>", listaAttivita(), $output);
+	$prenotazioneEffettuata = isset($done) ? "<div class=\"done\"><h2>" . $done . "</h2></div>" : "";
+	$output = str_replace("<h2 prenotazione/>", $prenotazioneEffettuata, $output);
+	$output = str_replace("<div activitySection/>", listaAttivita(), $output);
+	$output = str_replace("<div activityForm/>", formAttivita(), $output);
 
 
-    echo $output;
+
+	echo $output;
