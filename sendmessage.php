b6e17d1f5ad473dde4233c56b344a57959b2a96b
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-28 12:00:51 +0100

    Controlli base lato php in contacts.php

--- sendmessage.php
+++ sendmessage.php
@@ -8,17 +8,27 @@
 	}
 
 	if (isset($_POST["sendmessage"])) {
-		$name = mysqli_real_escape_string($connessione, sanitizeString($_POST['name']));
-		$email = mysqli_real_escape_string($connessione, sanitizeEmail($_POST['email']));
-		$message = mysqli_real_escape_string($connessione, sanitizeString($_POST['message']));
-		$cat = mysqli_real_escape_string($connessione, sanitizeString($_POST['subject']));
-		$today = date("Y-m-d");
-		$sql = ("INSERT INTO messaggio(Categoria_Mes,Nome_Mes,Mail_Mes,Testo_Mes,Data_Mes) VALUES ('$cat','$name','$email','$message','$today');");
-		if ($result = $connessione->query($sql)) {
-			header("Location: contacts.php?done");
+		if (!$name = mysqli_real_escape_string($connessione, sanitizeString($_POST['name']))) {
+			header("Location: contacts.php?error=" . urlencode("Inserisci un nome valido"));
+			exit();
+		} elseif (!$email = mysqli_real_escape_string($connessione, sanitizeEmail($_POST['email']))) {
+			header("Location: contacts.php?error=" . urlencode("Inserisci una mail valida"));
+			exit();
+		} elseif (!$message = mysqli_real_escape_string($connessione, sanitizeString($_POST['message']))) {
+			header("Location: contacts.php?error=" . urlencode("Inserisci un messaggio"));
+			exit();
+		} elseif (!$cat = mysqli_real_escape_string($connessione, sanitizeString($_POST['subject']))) {
+			header("Location: contacts.php?error=" . urlencode("Seleziona una categoria"));
 			exit();
 		} else {
-			header("Location: contacts.php?error=" . urlencode($connessione->error));
-			exit();
+			$today = date("Y-m-d");
+			$sql = ("INSERT INTO messaggio(Categoria_Mes,Nome_Mes,Mail_Mes,Testo_Mes,Data_Mes) VALUES ('$cat','$name','$email','$message','$today');");
+			if ($result = $connessione->query($sql)) {
+				header("Location: contacts.php?done");
+				exit();
+			} else {
+				header("Location: contacts.php?error=" . urlencode($connessione->error));
+				exit();
+			}
 		}
 	}
