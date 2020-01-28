9b2cc63e9ed4ff1073986efb3a5f22029f12cc34
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-01-28 04:13:39 +0100

    Orari e biglietti valido

--- schedule.php
+++ schedule.php
@@ -1,12 +1,11 @@
 <?php
     require_once "includes/header.php";
     require_once "includes/footer.php";
-	require_once "includes/buyticket.php";
     if (!isset($_SESSION))
     	session_start();
 
     if (isset($_SESSION["userName"])) {
-    	$formAcquisto = "<form method=\"post\" action=\"schedule.php\" id=\"ticket\">
+    	$formAcquisto = "<form method=\"post\" action=\"buyticket.php\" id=\"ticket\">
 			<fieldset>
 				<legend>Acquista biglietti</legend>
 				<table>
@@ -16,17 +15,59 @@
 					</tr>
 					<tr>
 						<td>Biglietto intero</td>
-						<td><input type=\"number\" name=\"intero\" min=\"0\" max=\"10\"/></td>
+						<td>
+							<select name=\"intero\">
+								<option value=\"0\">0</option>
+								<option value=\"1\">1</option>
+								<option value=\"2\">2</option>
+								<option value=\"3\">3</option>
+								<option value=\"4\">4</option>
+								<option value=\"5\">5</option>
+								<option value=\"6\">6</option>
+								<option value=\"7\">7</option>
+								<option value=\"8\">8</option>
+								<option value=\"9\">9</option>
+								<option value=\"10\">10</option>
+							</select>
+						</td>
 						<td>12.00€</td>
 					</tr>
 					<tr>
 						<td>Biglietto ridotto (under 10)</td>
-						<td><input type=\"number\" name=\"ridottoB\" min=\"0\" max=\"10\"/></td>
+						<td>
+							<select name=\"ridottoB\">
+								<option value=\"0\">0</option>
+								<option value=\"1\">1</option>
+								<option value=\"2\">2</option>
+								<option value=\"3\">3</option>
+								<option value=\"4\">4</option>
+								<option value=\"5\">5</option>
+								<option value=\"6\">6</option>
+								<option value=\"7\">7</option>
+								<option value=\"8\">8</option>
+								<option value=\"9\">9</option>
+								<option value=\"10\">10</option>
+							</select>
+						</td>
 						<td>8.00€</td>
 					</tr>
 					<tr>
 						<td>Biglietto ridotto (over 65)</td>
-						<td><input type=\"number\" name=\"ridottoA\" min=\"0\" max=\"10\"/></td>
+						<td>
+							<select name=\"ridottoA\">
+								<option value=\"0\">0</option>
+								<option value=\"1\">1</option>
+								<option value=\"2\">2</option>
+								<option value=\"3\">3</option>
+								<option value=\"4\">4</option>
+								<option value=\"5\">5</option>
+								<option value=\"6\">6</option>
+								<option value=\"7\">7</option>
+								<option value=\"8\">8</option>
+								<option value=\"9\">9</option>
+								<option value=\"10\">10</option>
+							</select>
+						</td>
 						<td>10.00€</td>
 					</tr>
 				</table>
@@ -42,7 +83,7 @@
 	$output = str_replace("<meta/>", file_get_contents("html/head.html"), $output);
     $output = str_replace("<div id=\"header\"></div>", Header::build(), $output);
     $output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
-    $acquistoEffettuato = isset($done) ? "<div class=\"done\"><h2>" . $done . "</h2></div>" : "";
+    $acquistoEffettuato = isset($_GET["result"]) ? "<div class=\"done\"><h2>" . urldecode($_GET["result"]) . "</h2></div>" : "";
     $output = str_replace("<h2 acquisto/>", $acquistoEffettuato, $output);
     $output = str_replace("<form acquisto/>", $formAcquisto, $output);
 
